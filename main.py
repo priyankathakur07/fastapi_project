3b047f70943de1de86cd55c161fbf982e4494c72
Priyanka Thakur <priyanka.thakur@thoughtwin.com>
2026-06-08 10:23:12 +0530

    product

--- main.py
+++ main.py
@@ -9,3 +9,35 @@ def home():
 @app.get("/add")
 def add(a:int,b:int):
     return{"sum":a+b}
+
+
+products = [
+    {
+        "id": 1,
+        "name": "Laptop",
+        "price": 50000,
+        "description": "Dell Laptop"
+    },
+    {
+        "id": 2,
+        "name": "Mobile",
+        "price": 20000,
+        "description": "Samsung Mobile"
+    },
+    {
+        "id": 3,
+        "name": "Headphones",
+        "price": 3000,
+        "description": "Wireless Headphones"
+    }
+]
+
+
+# Product List API
+@app.get("/products")
+def product_list():
+    return {
+        "success": True,
+        "count": len(products),
+        "data": products
+    }
